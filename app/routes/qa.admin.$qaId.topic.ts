b4004103983e7c4b4242f3835eb68a5b7c74eb68
Jan Monschke <janmonschke@fastmail.com>
2024-11-26 09:35:02 +0100

    real-time update for topic changes

--- app/routes/qa.admin.$qaId.topic.ts
+++ app/routes/qa.admin.$qaId.topic.ts
@@ -1,7 +1,9 @@
 import { ActionFunction, redirect } from "@remix-run/node";
 import { db } from "~/db.server";
 import { isQaAdmin } from "~/helpers/access";
+import { updateQaEvent } from "~/helpers/events";
 import { qaAdmin } from "~/helpers/routes";
+import { emitter } from "~/services/emitter.server";
 
 export const action: ActionFunction = async ({ request, params }) => {
   const qa = await isQaAdmin(params.qaId, request);
@@ -34,5 +36,6 @@ export const action: ActionFunction = async ({ request, params }) => {
       });
     }
   }
+  emitter.emit(updateQaEvent(qa.id), Date.now());
   return redirect(qaAdmin(qa.id));
 };
