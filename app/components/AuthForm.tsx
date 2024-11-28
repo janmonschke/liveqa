3e7eafd6186414aacf87d4c6d3b6ce5f424513f8
Jan Monschke <janmonschke@fastmail.com>
2024-11-28 12:47:24 +0100

    update loading state for auth forms

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -1,42 +1,50 @@
-import { Form } from "@remix-run/react";
+import { Form, useFetcher } from "@remix-run/react";
 import { Box, Button, Heading, TextField } from "@radix-ui/themes";
 import { CenteredMediumContaner } from "./CenteredMediumContainer";
 
-export const AuthForm: React.FC<{ actionLabel: string; headline: string }> = ({
-  actionLabel,
-  headline,
-}) => (
-  <CenteredMediumContaner>
-    <Heading>{headline}</Heading>
-    <Box>
-      <Form method="post">
-        <Box mt="2" mb="1">
-          <label htmlFor="name">Username:</label>
-        </Box>
-        <TextField.Root
-          type="text"
-          name="name"
-          id="name"
-          required
-          minLength={3}
-          mb="2"
-        />
-        <Box mt="2" mb="1">
-          <label htmlFor="password"> 
-            Password:
-          </label>
-        </Box>
-        <TextField.Root
-          type="password"
-          name="password"
-          id="password"
-          autoComplete="current-password"
-          required
-          minLength={4}
-          mb="4"
-        />
-          <Button>{actionLabel}</Button>
-      </Form>
-    </Box>
-  </CenteredMediumContaner>
-);
+type Props = {
+  actionLabel: string;
+  headline: string;
+};
+
+export function AuthForm({ actionLabel, headline }: Props) {
+  const fetcher = useFetcher();
+  const isLoading = fetcher.state !== "idle";
+  return (
+    <CenteredMediumContaner>
+      <Heading>{headline}</Heading>
+      <Box>
+        <fetcher.Form method="post">
+          <Box mt="2" mb="1">
+            <label htmlFor="name">Username:</label>
+          </Box>
+          <TextField.Root
+            type="text"
+            name="name"
+            id="name"
+            required
+            minLength={3}
+            mb="2"
+            disabled={isLoading}
+          />
+          <Box mt="2" mb="1">
+            <label htmlFor="password">Password:</label>
+          </Box>
+          <TextField.Root
+            type="password"
+            name="password"
+            id="password"
+            autoComplete="current-password"
+            required
+            minLength={4}
+            mb="4"
+            disabled={isLoading}
+          />
+          <Button disabled={isLoading} loading={isLoading}>
+            {actionLabel}
+          </Button>
+        </fetcher.Form>
+      </Box>
+    </CenteredMediumContaner>
+  );
+}
